5288f15baa18d8c60a1eb210b7e647036fd3fe29
Dilou89 <odil_cadiou@orange.fr>
2017-02-11 10:47:27 +0100

    vers 1 etape et etape 2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
-(function(){
-	"use strict";
-	var app = {
-		init:function(){
-			// let's go
-		},
-	};
 
+$(document).ready(function(){
+	
+	$.ajax({
+		url: "example.md",
 
-	$(document).ready(function(){
-		app.init();
+		success: function(mdText){
+
+			var converter = new showdown.Converter();
+			var htmlText = converter.makeHtml(mdText);
+			console.log(htmlText)
+			$("#app").append(htmlText); 
+		}
 	});
-})();
+	$(".blog").click(function(){
+		$.ajax({
+			datatype:"json",
+			url:"/menu.json",
+			success:function(json){
+				console.log(coucou);
+				
+			}
+		});
+
+	});
+});
